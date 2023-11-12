626adaf133bd9494405456e6175a641e84f9a9e5
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-11-12 18:17:05 +0100

    Indentation

--- ex7.sql
+++ ex7.sql
@@ -1,12 +1,11 @@
 /*Lister les entités et leurs pays d'origine*/
 
-SELECT entity.name, country.country
+SELECT entity.name,
+       country.country
 FROM entity
-INNER JOIN address 
-ON entity.id_address = address.id_address
-INNER JOIN country 
-ON address.countries = country.code
-WHERE address.address IS NOT NULL 
-AND address.address != ''
-AND country.code IS NOT NULL 
-AND country.code != ''
+INNER JOIN address ON entity.id_address = address.id_address
+INNER JOIN country ON address.countries = country.code
+WHERE address.address IS NOT NULL
+  AND address.address != ''
+  AND country.code IS NOT NULL
+  AND country.code != ''
