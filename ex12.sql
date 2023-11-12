32b5ee19e47e6c6b2e9be7d75ccc9053d5ff4f2e
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-11-12 18:20:47 +0100

    Indentation

--- ex12.sql
+++ ex12.sql
@@ -1,6 +1,7 @@
 /*Trouver le nombre d'entités associées à chaque intermédiaire*/
 
-SELECT intermediary.name AS "Intermediary", COUNT(assoc_inter_entity.entity) AS "Nombre_entité"
+SELECT intermediary.name AS "Intermediary",
+       COUNT(assoc_inter_entity.entity) AS "Nombre_entité"
 FROM assoc_inter_entity
 JOIN intermediary ON intermediary.id = assoc_inter_entity.inter
 GROUP BY intermediary.name
