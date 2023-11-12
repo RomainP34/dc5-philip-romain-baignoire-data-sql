85146c6ffb68ed2f4341ed50176e5a0e8fbd4ab3
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-11-12 16:27:55 +0100

    Correction

--- ex8.sql
+++ ex8.sql
@@ -1,12 +1,8 @@
 /*Trouver tous les officiers et intermédiaires qui ont une association*/
 
-SELECT entity.name, country.country
-FROM entity
-INNER JOIN address 
-ON entity.id_address = address.id_address
-INNER JOIN country 
-ON address.countries = country.code
-WHERE address.address IS NOT NULL 
-AND address.address != ''
-AND country.code IS NOT NULL 
-AND country.code != ''
+SELECT  officer.name as "Officer", assoc_inter_offi.assoc_type, intermediary.name as "Intermediary"
+FROM officer
+INNER JOIN intermediary 
+ON assoc_inter_offi.inter = intermediary.id
+INNER JOIN assoc_inter_offi 
+ON officer.id = assoc_inter_offi.officer
