336d7258c292e1fa7d47fa425692415f90284da6
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-11-12 18:18:05 +0100

    Indentation

--- ex8.sql
+++ ex8.sql
@@ -1,8 +1,8 @@
 /*Trouver tous les officiers et intermédiaires qui ont une association*/
 
-SELECT  officer.name as "Officer", assoc_inter_offi.assoc_type, intermediary.name as "Intermediary"
+SELECT officer.name AS "Officer",
+       assoc_inter_offi.assoc_type,
+       intermediary.name AS "Intermediary"
 FROM officer
-INNER JOIN intermediary 
-ON assoc_inter_offi.inter = intermediary.id
-INNER JOIN assoc_inter_offi 
-ON officer.id = assoc_inter_offi.officer
+INNER JOIN intermediary ON assoc_inter_offi.inter = intermediary.id
+INNER JOIN assoc_inter_offi ON officer.id = assoc_inter_offi.officer
