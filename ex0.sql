82f6bcc53e3aaeb4cc74c30a5cd3953af854b41d
Philip Romain <r_philip1@stu-digital-campus.fr>
2023-11-12 17:05:57 +0100

    Indentation

--- ex0.sql
+++ ex0.sql
@@ -1,3 +1,4 @@
 /*Trouvez tous les pays et les codes connus les afficher en tant que “pays” et “libellé”.*/
 
-SELECT code AS libellé, country AS pays FROM country
+SELECT code AS libellé, country AS pays 
+FROM country
